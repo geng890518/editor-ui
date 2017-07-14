7a96ac512c05eedd2bdc99bcebed5425fe58a1b3
geng890518 <genyyjj@163.com>
2017-07-14 19:19:24 +0800

    posting

--- src/redux/modules/editor.js
+++ src/redux/modules/editor.js
@@ -346,7 +346,6 @@ export default function reducer(state = initialState, action = {}) {
     case GETCOLLECTIONS:
       return {
         ...state,
-        posting: true,
         posted: false,
       };
     case GETCOLLECTIONS_SUCCESS:
